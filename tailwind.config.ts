6c71e4b47174173d77a99f1efbf35b95b43a1e83
Mikael <bernardesmikael@outlook.com>
2024-04-13 18:18:03 -0300

    config: colos and screens in Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,24 @@ const config: Config = {
 	content: [
 		"./src/**/*.{js,ts,jsx,tsx,mdx}"
 	],
-	theme: {},
+	theme: {
+		colors: {
+			BG100: "#FFFFFF",
+			BG300: "#F5F5F5",
+			Line: "#DEDEDE",
+			TXT100: "#242424",
+			TXT300: "#8A8B8F",
+			Primary: "#0EA8FF"
+		},
+		screens: {
+			"xs": { max: "429px" },
+			"sm": { min: "430px", max: "739px" },
+			"md": { min: "740px", max: "1279px" },
+			"base": { min: "1280px", max: "1439px" },
+			"lg": { min: "1440px", max: "1511px" },
+			"xl": { min: "1512px" }
+		}
+	},
 	plugins: [],
 };
 export default config;
