0ac5704e893f02b101abf526c07dafb9520149b1
Mikael <bernardesmikael@outlook.com>
2024-04-14 17:03:41 -0300

    feat: add responsiviness in Banner component

--- src/shared/layout/DefaultNavigationHeader/Banner/index.tsx
+++ src/shared/layout/DefaultNavigationHeader/Banner/index.tsx
@@ -1,6 +1,6 @@
 
 function Banner() {
-	return <div className="bg-[#a129a1] w-full xs:h-[76px]"/>;
+	return <div className="bg-[#a129a1] xs:w-full xs:h-[76px] sm:h-[126px] md:h-[200px] lg:h-[260px] xl:h-[260px] md:mx-24 lg:mx-40 xl:mx-56"/>;
 }
 
 export { Banner };
