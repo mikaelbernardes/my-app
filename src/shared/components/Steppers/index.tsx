50817180ebff951cc0a327ae77d10a476e273651
Mikael <bernardesmikael@outlook.com>
2024-04-20 00:00:29 -0300

    feat: add Stepper index by Stepper

---
+++ src/shared/components/Steppers/index.tsx
@@ -0,0 +1,11 @@
+import { StepperContent } from "./Content";
+import { StepperInfo } from "./Info";
+import { StepperRoot } from "./Root";
+
+const Stepper = {
+	Root: StepperRoot,
+	Info: StepperInfo,
+	Content: StepperContent
+};
+
+export { Stepper };
