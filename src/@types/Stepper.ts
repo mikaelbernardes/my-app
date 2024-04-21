04ddd9a19777555bc707642f7c725ddb3b7265ba
Mikael <bernardesmikael@outlook.com>
2024-04-20 23:51:07 -0300

    feat: add types of Stepper and StepperBuilder

--- src/@types/Stepper.ts
+++ src/@types/Stepper.ts
@@ -1,10 +1,20 @@
-
-export type Colors = "bg-Primary" | "bg-TXT100" | "bg-Line"
+import { StaticImport } from "next/dist/shared/lib/get-img-props";
 
 export interface StepperProps {
-    circleColor: Colors,
+    title: string,
+    subTitle: string,
+    timeLapse: string,
+    position: number,
+    content?: string
+    link?: string,
+    image?: string | StaticImport
+}
+
+export interface DataStepperBuilder {
     title: string,
     subTitle: string,
     timeLapse: string,
     content?: string
+    link?: string,
+    image?: string | StaticImport
 }
