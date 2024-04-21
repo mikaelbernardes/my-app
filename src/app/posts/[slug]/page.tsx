da4ac5e555f8e8d9d9eb0d11dc43bc0490c0fead
Mikael <bernardesmikael@outlook.com>
2024-04-21 14:59:31 -0300

    feat: add pages for all posts list and post with content (WIP)

---
+++ src/app/posts/[slug]/page.tsx
@@ -0,0 +1,21 @@
+import { getPost } from "@/app/_services/notion";
+import ReactMarkdown from "react-markdown";
+
+interface PropsSlug {
+    params: {
+        slug: string
+    }
+}
+
+export default async function Blog({ params }: PropsSlug) {
+
+
+	const post = await getPost(params.slug);
+
+	return (
+		<>
+			<h1>{post.title}</h1>
+			<ReactMarkdown>{post.content}</ReactMarkdown>
+		</>
+	);
+}
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,9 +1,33 @@
+"use server";
 import { DefaultNavigationHeader } from "@/shared/layout/DefaultNavigationHeader";
+import { getPosts } from "../_services/notion";
+import { CardAtualizationsTemplate } from "@/shared/components/Card/template/CardAtualizationsTemplate";
+import Logo from "@public/ampliLogo.jpeg";
 
-export default function Posts() {
+export default async function Posts() {
+
+	const posts = await getPosts();
+
+	console.log(posts);
+
 	return(
 		<>
 			<DefaultNavigationHeader />
+			<div>
+				{
+					posts.map((post) => (
+						<CardAtualizationsTemplate 
+							content=""
+							perfilImage={Logo}
+							perfilMarcationName={post.createdAt}
+							perfilName={post.title}
+							postAlt={post.title}
+							postImage={Logo}
+							key={post.id}
+						/>
+					))
+				}
+			</div>
 		</>
 	);
 }
