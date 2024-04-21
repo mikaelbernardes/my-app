04028fc9061541503c97137a9b0bf93ccbc08bc0
Mikael <bernardesmikael@outlook.com>
2024-04-20 23:50:06 -0300

    feat: add complete StepperRoot component

--- src/shared/components/Steppers/Root.tsx
+++ src/shared/components/Steppers/Root.tsx
@@ -6,7 +6,7 @@ interface StepperRootProps {
 function StepperRoot({ children }: StepperRootProps) {
 
 	return(
-		<div className="w-fit h-fit flex flex-col xs:gap-4">
+		<div className="w-fit xs:h-[100px] sm:h-[100px] md:h-[100px] lg:h-[140px] xl:h-[140px] flex flex-col xs:gap-4 sm:gap-4 md:gap-5 lg:gap-6 xl:gap-6">
 			{ children }
 		</div>
 	);
