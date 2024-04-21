e1fc857b3fddd2e09ea4df8e3c96971572421c9a
Mikael <bernardesmikael@outlook.com>
2024-04-21 12:26:11 -0300

    feat: structured page about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,21 +1,31 @@
 import { StepperBuilder } from "@/shared/class/StepperBuilder";
+import { StudiesSteppers, worksSteppers } from "@/shared/data/DataStepperBuilder";
 import { DefaultNavigationHeader } from "@/shared/layout/DefaultNavigationHeader";
 
 export default function About() {
 	return(
 		<>
 			<DefaultNavigationHeader />
-			<div className="xs:px-5 sm:px-20 md:px-32 lg:px-56 xl:px-72 pb-4 xl:pb-12">
-				<h2 
-					className={`
+			<main className="xs:px-5 sm:px-20 md:px-32 lg:px-56 xl:px-72 pb-4 xl:pb-12">
+				<section className="flex flex-col">
+					<h2 
+						className={`
 						w-fit xs:my-4 sm:my-4 md:my-6 lg:my-8 xl:my-10 xs:text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl 
 						bg-gradient-to-r from-Primary to-TXT300 bg-clip-text text-transparent 
 					`}
-				>Experiência</h2>
-				<section className="flex gap-3">
-					<StepperBuilder />
+					>Experiência</h2>
+					<StepperBuilder data={worksSteppers} />
 				</section>
-			</div>
+				<section className="flex flex-col mt-4">
+					<h2 
+						className={`
+						w-fit xs:my-4 sm:my-4 md:my-6 lg:my-8 xl:my-10 xs:text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl 
+						bg-gradient-to-r from-Primary to-TXT300 bg-clip-text text-transparent 
+					`}
+					>Graduações e Cursos</h2>
+					<StepperBuilder data={StudiesSteppers} />
+				</section>
+			</main>
 		</>
 	);
 }
