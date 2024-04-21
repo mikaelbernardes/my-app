ac25b2095f80ac1c611ffd924803569c9cfcf058
Mikael <bernardesmikael@outlook.com>
2024-04-20 23:50:42 -0300

    feat: add complete StepperContent component

--- src/shared/components/Steppers/Content.tsx
+++ src/shared/components/Steppers/Content.tsx
@@ -7,7 +7,7 @@ function StepperContent({ content } : StepperContentProps) {
 
 	return(
 		<div className="w-full h-fit">
-			<p className="xs:text-[10px]">{ content }</p>
+			<p className="xs:text-[10px] sm:text-[10px] md:text-xs lg:text-base xl:text-base">{ content }</p>
 		</div>
 	);
 }
