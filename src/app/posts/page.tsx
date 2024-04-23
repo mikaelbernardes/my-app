94f32677bbfa4f22af2145626f1c68834b71c990
Mikael <bernardesmikael@outlook.com>
2024-04-22 23:07:06 -0300

    feat: add title for posts

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,28 +1,26 @@
 "use server";
 import { CardPostsTemplate } from "@/shared/components/Card/template/CardPostsTemplate";
 import { DefaultNavigationHeader } from "@/shared/layout/DefaultNavigationHeader";
-import PerfilImg from "@public/perfil.jpg";
 import { getPosts } from "../_services/notion";
 
 export default async function Posts() {
 
 	const posts = await getPosts();
 
-	console.log(posts);
+	const filterAllPosts = posts.filter(post => post.tags[0] === "post");
 
 	return(
 		<>
 			<DefaultNavigationHeader />
 			<main className="xs:px-5 sm:px-20 md:px-32 lg:px-56 xl:px-72">
 				{
-					posts.map((post) => (
+					filterAllPosts.map((post) => (
 						<CardPostsTemplate
 							content={post.description}
 							createdAt={post.createdAt}
-							perfilImage={PerfilImg}
-							perfilName={post.title}
 							key={post.id}
 							slug={post.slug}
+							title={post.title}
 						/>
 					))
 				}
