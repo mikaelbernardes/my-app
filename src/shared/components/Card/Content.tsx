9c849d244b2bd415cfc8a58762abeffd46ed0a9b
Mikael <bernardesmikael@outlook.com>
2024-04-22 23:05:31 -0300

    feat: add title for posts

--- src/shared/components/Card/Content.tsx
+++ src/shared/components/Card/Content.tsx
@@ -1,12 +1,21 @@
 
 interface CardContentProps {
-    content: string
+    content: string,
+	title?: string
 }
 
-function CardContent({ content }: CardContentProps) {
+function CardContent({ content, title }: CardContentProps) {
 	return(
-		<div>
-			<p className="xs:text-[10px] sm:text-xs md:text-sm lg:text-base xl:text-base font-light text-TXT100 xs:mt-4 sm:mt-4 md:mt-4 lg:mt-6 xl:mt-6">{content}</p>
+		<div className="flex flex-col gap-1 ">
+			{
+				title ?
+					<h2 className="xs:text-xs sm:text-sm md:text-base lg:text-lg xl:text-xl font-light text-Primary xs:mt-4 sm:mt-4 md:mt-4 lg:mt-6 xl:mt-6">{title}</h2>
+					:
+					null
+			}
+			<p 
+				className={`xs:text-[10px] sm:text-xs md:text-sm lg:text-base xl:text-base font-light text-TXT100 ${title ? "xs:mt-4 sm:mt-4 md:mt-4 lg:mt-6 xl:mt-6" : ""}`}
+			>{content}</p>
 		</div>
 	);
 }
