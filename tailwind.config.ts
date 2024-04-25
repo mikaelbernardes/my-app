f33ef85c2c93481f349e7d8f8e6318eaa6c135ee
Mikael <bernardesmikael@outlook.com>
2024-04-25 20:56:33 -0300

    fix: exclud animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -24,26 +24,6 @@ const config: Config = {
 		extend: {
 			backgroundImage: {
 				perfil: "url(/perfil.jpg)"
-			},
-			keyframes: {
-				smokeOneKey : {
-					"0%": { bottom: "20px", opacity: "0" },
-					"40%": { bottom: "50px", opacity: ".5" },
-					"80%": { bottom: "80px", opacity: ".3" },
-					"100%": { bottom: "80px", opacity: "0" }
-				},
-				smokeTwoKey : {
-					"0%": { bottom: "40px", opacity: "0" },
-					"40%": { bottom: "70px", opacity: ".5" },
-					"80%": { bottom: "80px", opacity: ".3" },
-					"100%": { bottom: "80px", opacity: "0" }
-				}
-			},
-			animation: {
-				smokeOne: "smokeOneKey 3s 4s linear infinite",
-				smokeTwo : "smokeTwoKey 3s 5s linear infinite",
-				smokeThree : "smokeTwoKey 3s 6s linear infinite",
-				smokeFour : "smokeOneKey 3s 5s linear infinite",
 			}
 		}
 	},
