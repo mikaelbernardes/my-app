6f841d10ac8d2ce1c62b22c92b23d2f0e2324cc2
Mikael <bernardesmikael@outlook.com>
2024-04-24 22:45:41 -0300

    fix: spaces in file

--- src/app/_services/typeNotion.ts
+++ src/app/_services/typeNotion.ts
@@ -1,131 +1,131 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 export interface NotionDatabaseResponse {
-    object: string;
-    results: Result[];
-    next_cursor: any;
-    has_more: boolean;
-    type: string;
-    page_or_database: PageOrDatabase;
-  }
-  
+  object: string;
+  results: Result[];
+  next_cursor: any;
+  has_more: boolean;
+  type: string;
+  page_or_database: PageOrDatabase;
+}
+
 export interface Result {
-    object: string;
-    id: string;
-    created_time: string;
-    last_edited_time: string;
-    created_by: CreatedBy;
-    last_edited_by: LastEditedBy;
-    cover: any;
-    icon: any;
-    parent: Parent;
-    archived: boolean;
-    properties: Properties;
-    url: string;
-    public_url: any;
-  }
-  
+  object: string;
+  id: string;
+  created_time: string;
+  last_edited_time: string;
+  created_by: CreatedBy;
+  last_edited_by: LastEditedBy;
+  cover: any;
+  icon: any;
+  parent: Parent;
+  archived: boolean;
+  properties: Properties;
+  url: string;
+  public_url: any;
+}
+
 export interface CreatedBy {
-    object: string;
-    id: string;
-  }
-  
+  object: string;
+  id: string;
+}
+
 export interface LastEditedBy {
-    object: string;
-    id: string;
-  }
-  
+  object: string;
+  id: string;
+}
+
 export interface Parent {
-    type: string;
-    database_id: string;
-  }
-  
+  type: string;
+  database_id: string;
+}
+
 export interface Properties {
-    tags: Tags;
-    slug: Slug;
-    title: Title;
-    description: Descriptions
-  }
-  
+  tags: Tags;
+  slug: Slug;
+  title: Title;
+  description: Descriptions
+}
+
 export interface Tags {
-    id: string;
-    type: string;
-    multi_select: MultiSelect[];
-  }
-  
-  interface RichTextDescription {
-    type: string;
-    text: object;
-    annotations: object;
-    plain_text: string;
-    href: string | null
-  }
-  
+  id: string;
+  type: string;
+  multi_select: MultiSelect[];
+}
+
+interface RichTextDescription {
+  type: string;
+  text: object;
+  annotations: object;
+  plain_text: string;
+  href: string | null
+}
+
 export interface Descriptions {
-    id: string;
-    type: string;
-    rich_text: RichTextDescription[]
-  }
-  
+  id: string;
+  type: string;
+  rich_text: RichTextDescription[]
+}
+
 export interface MultiSelect {
-    id: string;
-    name: string;
-    color: string;
-  }
-  
+  id: string;
+  name: string;
+  color: string;
+}
+
 export interface Slug {
-    id: string;
-    type: string;
-    rich_text: RichText[];
-  }
-  
+  id: string;
+  type: string;
+  rich_text: RichText[];
+}
+
 export interface RichText {
-    type: string;
-    text: Text;
-    annotations: Annotations;
-    plain_text: string;
-    href: any;
-  }
-  
+  type: string;
+  text: Text;
+  annotations: Annotations;
+  plain_text: string;
+  href: any;
+}
+
 export interface Text {
-    content: string;
-    link: any;
-  }
-  
+  content: string;
+  link: any;
+}
+
 export interface Annotations {
-    bold: boolean;
-    italic: boolean;
-    strikethrough: boolean;
-    underline: boolean;
-    code: boolean;
-    color: string;
-  }
-  
+  bold: boolean;
+  italic: boolean;
+  strikethrough: boolean;
+  underline: boolean;
+  code: boolean;
+  color: string;
+}
+
 export interface Title {
-    id: string;
-    type: string;
-    title: Title2[];
-  }
-  
+  id: string;
+  type: string;
+  title: Title2[];
+}
+
 export interface Title2 {
-    type: string;
-    text: Text2;
-    annotations: Annotations2;
-    plain_text: string;
-    href: any;
-  }
-  
+  type: string;
+  text: Text2;
+  annotations: Annotations2;
+  plain_text: string;
+  href: any;
+}
+
 export interface Text2 {
-    content: string;
-    link: any;
-  }
-  
+  content: string;
+  link: any;
+}
+
 export interface Annotations2 {
-    bold: boolean;
-    italic: boolean;
-    strikethrough: boolean;
-    underline: boolean;
-    code: boolean;
-    color: string;
-  }
-  
+  bold: boolean;
+  italic: boolean;
+  strikethrough: boolean;
+  underline: boolean;
+  code: boolean;
+  color: string;
+}
+
 export interface PageOrDatabase { }
