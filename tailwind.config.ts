90d3dd47871c85f75d8d7005e3f5b0f6ffc782f7
Mikael <bernardesmikael@outlook.com>
2024-04-25 20:56:09 -0300

    feat: add transparent color in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,8 @@ const config: Config = {
 			Line: "#BDC5CD",
 			TXT100: "#141619",
 			TXT300: "#687684",
-			Primary: "#4C9EEB"
+			Primary: "#4C9EEB",
+			transparent: "transparent"
 		},
 		screens: {
 			"xs": { max: "429px" },
@@ -23,6 +24,26 @@ const config: Config = {
 		extend: {
 			backgroundImage: {
 				perfil: "url(/perfil.jpg)"
+			},
+			keyframes: {
+				smokeOneKey : {
+					"0%": { bottom: "20px", opacity: "0" },
+					"40%": { bottom: "50px", opacity: ".5" },
+					"80%": { bottom: "80px", opacity: ".3" },
+					"100%": { bottom: "80px", opacity: "0" }
+				},
+				smokeTwoKey : {
+					"0%": { bottom: "40px", opacity: "0" },
+					"40%": { bottom: "70px", opacity: ".5" },
+					"80%": { bottom: "80px", opacity: ".3" },
+					"100%": { bottom: "80px", opacity: "0" }
+				}
+			},
+			animation: {
+				smokeOne: "smokeOneKey 3s 4s linear infinite",
+				smokeTwo : "smokeTwoKey 3s 5s linear infinite",
+				smokeThree : "smokeTwoKey 3s 6s linear infinite",
+				smokeFour : "smokeOneKey 3s 5s linear infinite",
 			}
 		}
 	},
