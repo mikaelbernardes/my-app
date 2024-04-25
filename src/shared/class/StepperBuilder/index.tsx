fc6ecb2a0965199ff7e07342a314a0b29384a1c8
Mikael <bernardesmikael@outlook.com>
2024-04-24 22:46:41 -0300

    fix: transform in class builder

--- src/shared/class/StepperBuilder/index.tsx
+++ src/shared/class/StepperBuilder/index.tsx
@@ -1,36 +1,78 @@
+"use client";
+import React, { Component, ReactNode } from "react";
 import { DataStepperBuilder } from "@/@types/Stepper";
 import { StepperTemplate } from "../../components/Steppers/template/StepperTemplate";
 import { StepperLine } from "./StepperLine";
 
 interface StepperBuilderProps {
-	data: DataStepperBuilder[]
+    data: DataStepperBuilder[]
 }
 
-function StepperBuilder({ data } : StepperBuilderProps) {
+interface StepperBuilderState {
+    stepperLines: ReactNode[];
+}
 
-	const stepperLines = data.map((_, index) => <StepperLine key={index} position={index} size={data.length} />);
+class StepperBuilder extends Component<StepperBuilderProps, StepperBuilderState> {
+	constructor(props: StepperBuilderProps) {
+		super(props);
+		this.state = {
+			stepperLines: [],
+		};
+	}
 
-	return (
-		<section className="flex flex-col gap-8">
-			{stepperLines.map((line, index) => (
-				<div className="flex xs:gap-2 sm:gap-2 md:gap-3 lg:gap-4 xl:gap-4" key={index}>
-					{line}
-					<div className="flex flex-col items-center">
-						<StepperTemplate
-							position={index}
-							content={data[index].content}
-							subTitle={data[index].subTitle}
-							title={data[index].title}
-							timeLapse={data[index].timeLapse}
-							image={data[index].image}
-							link={data[index].link}
-						/>
-					</div>
+	componentDidMount() {
+		this.buildStepperLines();
+	}
+
+	componentDidUpdate(prevProps: StepperBuilderProps) {
+		if (prevProps.data !== this.props.data) {
+			this.buildStepperLines();
+		}
+	}
+
+	private buildStepperLines() {
+		const { data } = this.props;
+		const stepperLines = data.map((_, index) => (
+			<div className="flex xs:gap-2 sm:gap-2 md:gap-3 lg:gap-4 xl:gap-4" key={index}>
+				<StepperLine position={index} size={data.length} />
+				<div className="flex flex-col items-center">
+					<StepperTemplate
+						position={index}
+						content={data[index].content}
+						subTitle={data[index].subTitle}
+						title={data[index].title}
+						timeLapse={data[index].timeLapse}
+						image={data[index].image}
+						link={data[index].link}
+					/>
 				</div>
-			))}
-		</section>
-	);
+			</div>
+		));
+		this.setState({ stepperLines });
+	}
+
+	render(): ReactNode {
+		const { stepperLines } = this.state;
+
+		return (
+			<section className="flex flex-col gap-8">
+				{stepperLines}
+			</section>
+		);
+	}
 }
 
-export { StepperBuilder };
+class StepperBuilderBuilder {
+	private data: DataStepperBuilder[] = [];
 
+	withData(data: DataStepperBuilder[]): StepperBuilderBuilder {
+		this.data = data;
+		return this;
+	}
+
+	build(): ReactNode {
+		return <StepperBuilder data={this.data} />;
+	}
+}
+
+export { StepperBuilder, StepperBuilderBuilder };
