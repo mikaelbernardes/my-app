36c838f8bdc752c0e326368fbd8038fb767dc433
Mikael <bernardesmikael@outlook.com>
2024-04-26 15:30:28 -0300

    feat: create a loading for post page

---
+++ src/app/posts/[slug]/loading.tsx
@@ -0,0 +1,32 @@
+
+export default function Loading() {
+
+	return(
+		<div className="w-full h-fit flex flex-col items-center py-5">
+			<div className="animate-pulse flex flex-col items-start gap-4 w-8/12">
+				<div>
+					<div className="w-48 h-6 bg-Line rounded-md"></div>
+					<div className="w-28 h-4 bg-Line mx-auto mt-3 rounded-md"></div>
+				</div>
+				<div className="h-7 bg-Line w-full rounded-md"></div>
+				<div className="h-7 bg-Line w-full rounded-md"></div>
+				<div className="h-7 bg-Line w-full rounded-md"></div>
+				<div className="h-7 bg-Line w-full rounded-md"></div>
+				<div className="h-7 bg-Line w-full rounded-md"></div>
+				<div className="h-7 bg-Line w-full rounded-md"></div>
+				<div className="h-7 bg-Line w-full rounded-md"></div>
+				<div className="h-7 bg-Line w-full rounded-md"></div>
+				<div className="h-7 bg-Line w-full rounded-md"></div>
+				<div className="h-7 bg-Line w-full rounded-md"></div>
+				<div className="h-7 bg-Line w-full rounded-md"></div>
+				<div className="h-7 bg-Line w-full rounded-md"></div>
+				<div className="h-7 bg-Line w-full rounded-md"></div>
+				<div className="h-7 bg-Line w-full rounded-md"></div>
+				<div className="h-7 bg-Line w-full rounded-md"></div>
+				<div className="h-7 bg-Line w-full rounded-md"></div>
+
+				<div className="h-7 bg-Line w-1/2 rounded-md"></div>
+			</div>
+		</div>
+	);
+}
