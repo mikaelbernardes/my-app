5335115c1753ca6c0adf0f55de2fa56ba5c21e60
Mikael <bernardesmikael@outlook.com>
2024-04-26 17:33:36 -0300

    feat: name in links

--- src/shared/data/DataSocialMediaLinks.ts
+++ src/shared/data/DataSocialMediaLinks.ts
@@ -6,21 +6,25 @@ export const SocialMediaLinksData: SocialMediaLinksProps[] = [
 	{
 		icon: FaGithub,
 		link: "https://github.com/mikaelbernardes",
-		color: "hover:text-[#2B3137]"
+		color: "hover:text-[#2B3137]",
+		name: "Github"
 	},
 	{
 		icon: FaLinkedin,
 		link: "https://www.linkedin.com/in/bernardesmikael/",
-		color: "hover:text-[#0E76A8]"
+		color: "hover:text-[#0E76A8]",
+		name: "Linkedin"
 	},
 	{
 		icon: FaBehanceSquare,
 		link: "https://www.behance.net/mikaelbernardes1",
-		color: "hover:text-[#053eff]"
+		color: "hover:text-[#053eff]",
+		name: "Behance"
 	},
 	{
 		icon: BiLogoGmail,
 		link: "mailto:mikaelbernardes2022@gmail.com",
-		color: "hover:text-[#f2a60c]"
+		color: "hover:text-[#f2a60c]",
+		name: "Gmail"
 	}
 ];
